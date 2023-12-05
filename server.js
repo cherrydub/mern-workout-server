d620234922bbc27a0d120cf5acdeb3da407ad96d
cherrydub <cherrydub@gmail.com>
2023-12-05 00:18:46 +0000

    adding main page msg

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use("/", (req, res) => {
+  res.json({ msg: "welcome to the main page" });
+});
+
 app.use("/api/workouts", workoutRoutes);
 
 app.use("/api/user", userRoutes);
